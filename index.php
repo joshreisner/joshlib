783ac1f3f0a2c8ac947c0a7858813c3099fffc69
joshreisner <josh@joshreisner.com>
2009-03-04 03:10:43 +0000

    this hmac thing is driving me crazy still

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 http://code.google.com/p/joshlib/ (wiki / documentation / tracking)
 http://joshlib.joshreisner.com/ (eventual website)
 
-this hmac thing is drivin
+this hmac thing is drivi
 */
 $_josh["time_start"] = microtime(true);	//start the processing time stopwatch -- use format_time_exec() to access this
 
