0ffec726399e40341ba03b236bc8098bc4c66352
joshreisner <josh@joshreisner.com>
2009-04-27 03:38:24 +0000

    today's updates

--- javascript.js
+++ javascript.js
@@ -2,6 +2,14 @@
 
 	this file is generated automatically -- only edit through joshlib SVN
 	
+	contains the following sections
+	
+	css
+	form
+	format
+	map
+	url
+	window
 */
 
 /* css */
@@ -53,6 +61,8 @@ function form_tinymce_init(cssLocation) {
 		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,separator,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,separator,undo,redo,separator,link,unlink,image,|,code",
 		theme_advanced_buttons2 : "",
 		theme_advanced_buttons3 : "",
+		theme_advanced_resizing : true,		
+		theme_advanced_statusbar_location : "bottom",
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "center",
 		extended_valid_elements : "a[href|target],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[align|style],p[align]",
