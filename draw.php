e45701eeeaf2a20a855ebb0ec81bb5266ef6f357
joshreisner <josh@joshreisner.com>
2009-07-27 04:22:34 +0000

    more stuff -- tinymce image uploading

--- draw.php
+++ draw.php
@@ -446,7 +446,7 @@ function draw_javascript($javascript=false) {
 	';
 }
 
-function draw_javascript_tinymce($path_css="/styles/tinymce.css", $path_script="/_site/tinymce/jscripts/tiny_mce/tiny_mce.js") {
+function draw_javascript_tinymce($path_css="/styles/tinymce.css", $path_script="/_site/tiny_mce/tiny_mce.js") {
 	return draw_javascript_src() . draw_javascript_src($path_script) . draw_javascript("form_tinymce_init('" . $path_css . "');");
 }
 
--- javascript.js
+++ javascript.js
@@ -105,43 +105,25 @@ function form_tinymce_clear(field_id) {
 	tinyMCE.getInstanceById(field_id).getBody().innerHTML='';
 }
 
-function form_tinymce_init(cssLocation, imageManager) {
-	if (imageManager) {
-		tinyMCE.init({
-			mode : "textareas",
-			theme : "advanced",
-			skin : "default",
-			theme_advanced_buttons1 : "bold,italic,underline,strikethrough,separator,justifyleft,justifycenter,blockquote,separator,bullist,numlist,separator,undo,redo,separator,link,unlink,image,|,code",
-			theme_advanced_buttons2 : "",
-			theme_advanced_buttons3 : "",
-			theme_advanced_resizing : true,		
-			//theme_advanced_statusbar_location : "bottom",
-			theme_advanced_toolbar_location : "top",
-			theme_advanced_toolbar_align : "center",
-			extended_valid_elements : "a[href|target|rel],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[align|style],p[align]",
-			content_css : cssLocation + "?" + new Date().getTime(),
-			plugins : "imagemanager",
-			editor_selector : "mceEditor",
-			editor_deselector : "mceNoEditor"
-		});
-	} else {
-		tinyMCE.init({
-			mode : "textareas",
-			theme : "advanced",
-			skin : "default",
-			theme_advanced_buttons1 : "bold,italic,underline,strikethrough,separator,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,separator,undo,redo,separator,link,unlink,image,|,code",
-			theme_advanced_buttons2 : "",
-			theme_advanced_buttons3 : "",
-			theme_advanced_resizing : true,		
-			//theme_advanced_statusbar_location : "bottom",
-			theme_advanced_toolbar_location : "top",
-			theme_advanced_toolbar_align : "center",
-			extended_valid_elements : "a[href|target|rel],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[align|style],p[align]",
-			content_css : cssLocation + "?" + new Date().getTime(),
-			editor_selector : "mceEditor",
-			editor_deselector : "mceNoEditor"
-		});
-	}
+function form_tinymce_init(cssLocation) {
+	tinyMCE.init({
+		mode : "textareas",
+		theme : "advanced",
+		skin : "default",
+		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,separator,justifyleft,justifycenter,blockquote,separator,bullist,numlist,separator,undo,redo,separator,link,unlink,image,|,code",
+		theme_advanced_buttons2 : "",
+		theme_advanced_buttons3 : "",
+		theme_advanced_resizing : true,		
+		//theme_advanced_statusbar_location : "bottom",
+		theme_advanced_toolbar_location : "top",
+		theme_advanced_toolbar_align : "center",
+		extended_valid_elements : "a[href|target|rel],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[align|style],p[align]",
+		content_css : cssLocation + "?" + new Date().getTime(),
+		plugins : "imagemanager",
+		editor_selector : "mceEditor",
+		editor_deselector : "mceNoEditor",
+		relative_urls : false
+	});
 }
 
 function form_checkboxes_empty(form, name) {
