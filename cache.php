4d00b37d81e3c89cdd2e97637c259e3898cf7d7b
joshreisner <josh@joshreisner.com>
2009-08-27 12:46:21 +0000

    working on caching

--- cache.php
+++ cache.php
@@ -19,22 +19,23 @@ function cache_end() {
 	file_put($_josh['cache'], $contents);
 }
 
-function cache_start($user=false) {
+function cache_start($page=false) {
 	global $_josh;
 	
-	//determine what filename we should use
+	//determine what filename we should use--defaults to path_query
 	$filename = $_josh['write_folder'] . '/caches/';
-	if ($user && !empty($_SESSION['user_id'])) $filename .= $_SESSION['user_id'] . '/';
-	$filename .= urlencode($_josh['request']['path_query']);
+	if (!empty($_SESSION['user_id'])) $filename .= $_SESSION['user_id'] . '/';
+	$filename .= urlencode((($page) ? $page : $_josh['request']['path_query']));
 
 	if (file_is($filename)) {
 		//get cache file
 		echo file_get($filename);
-		return false; //ie you don't need to continue processing, because i've got a cache here
+		return false; //meaning you don't need to continue processing, because i've got a cache here
 	} else {
+		//or create cache file
 		$_josh['cache'] = $filename;
 		ob_start();
-		return true; //ie you do need to process
+		return true; //meaning you do need to process
 	}
 }
 
