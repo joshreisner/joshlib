1a6891aa69086f7c76ac87c3ac6490b59f9f2cb8
joshreisner <josh@joshreisner.com>
2010-11-05 14:55:55 +0000

    debugging swiftmailer

--- index.php
+++ index.php
@@ -184,7 +184,7 @@ define('TIME_START', microtime(true));	//start the processing time stopwatch -- 
 		file_dir_writable();
 		file_put_config();
 	}
-	require(DIRECTORY_ROOT . $_josh['config']);
+	if (!include(DIRECTORY_ROOT . $_josh['config'])) die('Could not create config file, please check the root folder\'s permissions.');
 	
 //check to make sure we're on the correct domain, might have read host variable from config file
 	if (isset($_josh['host']) && ($_josh['host'] != $_josh['request']['host'])) url_change($_josh['request']['protocol'] . '://' . $_josh['host'] . $_josh['request']['path_query']);
