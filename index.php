ccd7f20df516537eef38b57d6811c45814523a78
joshreisner <josh@joshreisner.com>
2011-04-04 22:37:38 +0000

    tinymce editor defaults

--- index.php
+++ index.php
@@ -517,62 +517,77 @@ function lib_get($string) {
 			//special statements for tinymce
 			$return = lib_get('jquery') . draw_javascript('
 				$(function(){
-					$("textarea.tinymce").tinymce({
-						// Location of TinyMCE script
-						script_url : "' . str_replace('jquery.tinymce.js', 'tiny_mce.js', lib_location('tinymce')) . '",
-				
-						//legacy code
-						theme : "advanced",
-						
-						theme_advanced_buttons1 : "' . (user() ? 
-							'styleselect,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,blockquote,|,bullist,numlist,outdent,indent,|,link,unlink,image,|,code' : 
-							'bold,italic,underline,strikethrough,|,justifyleft,justifycenter,blockquote,|,bullist,numlist,|,link,unlink') . '",
-						theme_advanced_buttons2 : "",
-						theme_advanced_resizing : true,		
-						theme_advanced_toolbar_location : "top",
-						
-						valid_elements : "@[id|class|style|title|dir<ltr?rtl|lang|xml::lang|onclick|ondblclick|"
-							+ "onmousedown|onmouseup|onmouseover|onmousemove|onmouseout|onkeypress|"
-							+ "onkeydown|onkeyup],a[rel|rev|charset|hreflang|tabindex|accesskey|type|"
-							+ "name|href|target|title|class|onfocus|onblur],strong/b,em/i,strike,u,"
-							+ "#p,-ol[type|compact],-ul[type|compact],-li,br,img[longdesc|usemap|"
-							+ "src|border|alt=|title|hspace|vspace|width|height|align],-sub,-sup,"
-							+ "-blockquote,-table[border=0|cellspacing|cellpadding|width|frame|rules|"
-							+ "height|align|summary|bgcolor|background|bordercolor],-tr[rowspan|width|"
-							+ "height|align|valign|bgcolor|background|bordercolor],tbody,thead,tfoot,"
-							+ "#td[colspan|rowspan|width|height|align|valign|bgcolor|background|bordercolor"
-							+ "|scope],#th[colspan|rowspan|width|height|align|valign|scope],caption,-div,"
-							+ "-span,-code,-pre,address,-h1,-h2,-h3,-h4,-h5,-h6,hr[size|noshade],-font[face"
-							+ "|size|color],dd,dl,dt,cite,abbr,acronym,del[datetime|cite],ins[datetime|cite],"
-							+ "object[classid|width|height|codebase|*],param[name|value|_value],embed[type|width"
-							+ "|height|src|*],script[src|type],map[name],area[shape|coords|href|alt|target],bdo,"
-							+ "button,col[align|char|charoff|span|valign|width],colgroup[align|char|charoff|span|"
-							+ "valign|width],dfn,fieldset,form[action|accept|accept-charset|enctype|method],"
-							+ "input[accept|alt|checked|disabled|maxlength|name|readonly|size|src|type|value],"
-							+ "kbd,label[for],legend,noscript,optgroup[label|disabled],option[disabled|label|selected|value],"
-							+ "q[cite],samp,select[disabled|multiple|name|size],small,"
-							+ "textarea[cols|rows|disabled|name|readonly],tt,var,big",
+					$("textarea.tinymce").each(function(){
+						$(this).tinymce({
+							// Location of TinyMCE script
+							script_url : "' . str_replace('jquery.tinymce.js', 'tiny_mce.js', lib_location('tinymce')) . '",
+					
+							//legacy code
+							theme : "advanced",
+							
+							theme_advanced_buttons1 : "' . (user() ? 
+								'styleselect,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,blockquote,|,bullist,numlist,outdent,indent,|,link,unlink,image,|,code' : 
+								'bold,italic,underline,strikethrough,|,justifyleft,justifycenter,blockquote,|,bullist,numlist,|,link,unlink') . '",
+							theme_advanced_buttons2 : "",
+							theme_advanced_resizing : true,		
+							theme_advanced_toolbar_location : "top",
+							
+							valid_elements : "@[id|class|style|title|dir<ltr?rtl|lang|xml::lang|onclick|ondblclick|"
+								+ "onmousedown|onmouseup|onmouseover|onmousemove|onmouseout|onkeypress|"
+								+ "onkeydown|onkeyup],a[rel|rev|charset|hreflang|tabindex|accesskey|type|"
+								+ "name|href|target|title|class|onfocus|onblur],strong/b,em/i,strike,u,"
+								+ "#p,-ol[type|compact],-ul[type|compact],-li,br,img[longdesc|usemap|"
+								+ "src|border|alt=|title|hspace|vspace|width|height|align],-sub,-sup,"
+								+ "-blockquote,-table[border=0|cellspacing|cellpadding|width|frame|rules|"
+								+ "height|align|summary|bgcolor|background|bordercolor],-tr[rowspan|width|"
+								+ "height|align|valign|bgcolor|background|bordercolor],tbody,thead,tfoot,"
+								+ "#td[colspan|rowspan|width|height|align|valign|bgcolor|background|bordercolor"
+								+ "|scope],#th[colspan|rowspan|width|height|align|valign|scope],caption,-div,"
+								+ "-span,-code,-pre,address,-h1,-h2,-h3,-h4,-h5,-h6,hr[size|noshade],-font[face"
+								+ "|size|color],dd,dl,dt,cite,abbr,acronym,del[datetime|cite],ins[datetime|cite],"
+								+ "object[classid|width|height|codebase|*],param[name|value|_value],embed[type|width"
+								+ "|height|src|*],script[src|type],map[name],area[shape|coords|href|alt|target],bdo,"
+								+ "button,col[align|char|charoff|span|valign|width],colgroup[align|char|charoff|span|"
+								+ "valign|width],dfn,fieldset,form[action|accept|accept-charset|enctype|method],"
+								+ "input[accept|alt|checked|disabled|maxlength|name|readonly|size|src|type|value],"
+								+ "kbd,label[for],legend,noscript,optgroup[label|disabled],option[disabled|label|selected|value],"
+								+ "q[cite],samp,select[disabled|multiple|name|size],small,"
+								+ "textarea[cols|rows|disabled|name|readonly],tt,var,big",
+	
+							extended_valid_elements : "a[href|target|rel|name],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|style],dir,hr[class|width|size|noshade],font[face|size|color|style],span[align|class|style],p[align|class|style],table[cellspacing,align,border,cellpadding,class],tr[class],td[width,align,class]",
+							content_css : "/styles/tinymce.css?" + new Date().getTime(),
+							plugins : "' . (user() ? 'imagemanager,filemanager,' : '') . 'paste",
+							
+							
+							//remove shortcuts
+							custom_shortcuts : 0,
+							
+							setup : function(ed) {
+								//ed.addShortcut("ctrl+1", "Some description", function() { alert("Whee!"); });
+					        },
 
-						extended_valid_elements : "a[href|target|rel|name],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|style],dir,hr[class|width|size|noshade],font[face|size|color|style],span[align|class|style],p[align|class|style],table[cellspacing,align,border,cellpadding,class],tr[class],td[width,align,class]",
-						content_css : "/styles/tinymce.css?" + new Date().getTime(),
-						plugins : "' . (user() ? 'imagemanager,filemanager,' : '') . 'paste",
-						
-						relative_urls : false,
-						remove_script_host : false /*we need this again for lc backend */
-				
-						/* new stuff in this version
-						// Drop lists for link/image/media/template dialogs
-						template_external_list_url : "lists/template_list.js",
-						external_link_list_url : "lists/link_list.js",
-						external_image_list_url : "lists/image_list.js",
-						media_external_list_url : "lists/media_list.js",
-				
-						// Replace values for the template plugin
-						template_replace_values : {
-							username : "Some User",
-							staffid : "991234"
-						}*/
+							
+							relative_urls : false,
+							remove_script_host : false /*we need this again for lc backend */
+					
+							/* new stuff in this version
+							// Drop lists for link/image/media/template dialogs
+							template_external_list_url : "lists/template_list.js",
+							external_link_list_url : "lists/link_list.js",
+							external_image_list_url : "lists/image_list.js",
+							media_external_list_url : "lists/media_list.js",
+					
+							// Replace values for the template plugin
+							template_replace_values : {
+								username : "Some User",
+								staffid : "991234"
+							}*/
+						});
+						//tinyMCE.get($(this).attr("id")).addShortcut("ctrl+alt+shift+o", "Some description", function() { alert("Whee!"); });
 					});
+					
+					
+
 				});
 			') . $return;
 			file_dir_writable('images');
