e255a7a4e75e92d2d8dd945875a6de7d93113162
weerd <diego.o.lorenzo@gmail.com>
2011-08-08 21:07:51 +0000

    JoshLib: added ie class to all conditional comments

--- draw.php
+++ draw.php
@@ -267,11 +267,11 @@ function draw_dl($array, $class=false) {
 
 function draw_doctype($lang='en') {
 	return '<!DOCTYPE html>
-        <!--[if IEMobile 7 ]><html class="no-js iem7" lang="' . $lang . '" manifest="default.appcache?v=1"><![endif]-->
-        <!--[if lt IE 7 ]><html class="no-js ie6" lang="' . $lang . '"><![endif]-->
-        <!--[if IE 7 ]><html class="no-js ie7" lang="' . $lang . '"><![endif]-->
-        <!--[if IE 8 ]><html class="no-js ie8" lang="' . $lang . '"><![endif]-->
-        <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="' . $lang . '"><!--<![endif]-->';
+        <!--[if IEMobile 7 ]><html class="no-js ie iem7" lang="' . $lang . '" manifest="default.appcache?v=1"><![endif]-->
+        <!--[if lt IE 7 ]><html class="no-js ie ie6" lang="' . $lang . '"><![endif]-->
+        <!--[if IE 7 ]><html class="no-js ie ie7" lang="' . $lang . '"><![endif]-->
+        <!--[if IE 8 ]><html class="no-js ie ie8" lang="' . $lang . '"><![endif]-->
+        <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js ie" lang="' . $lang . '"><!--<![endif]-->';
 }
 
 function draw_dump($var, $forceType='', $bCollapsed=false) {
