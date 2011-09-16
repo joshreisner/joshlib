4a16383705781065afd699cd3379718519d00e35
joshreisner <josh@joshreisner.com>
2011-09-16 21:23:26 +0000

    playing around with html class

--- html.php
+++ html.php
@@ -1,31 +1,29 @@
 <?php
 error_debug('including html.php', __file__, __line__);
-//i'm going to very carefully rewrite most of the draw.php in here
 
-function html_a($link=false, $href='#', $arguments=false, $new_window=false, $max_length=60) {
-	//$max_length is the maximum length of the $link if it's empty, eg <a href="http://www.areallylongaddress.com/foo/bar/">http://www.areallylong...</a>
-	if (!$link) {
-		$link = $href;
-		if (strlen($link) > $max_length) $link = substr($str, 0, $max_length) . '&hellip;';
-	}
-	$arguments = array_arguments($arguments);
-	if (format_text_starts('mailto:', $href)) {
-		//obfuscate the address to fight spam
-		$link = format_ascii(str_replace('mailto:', '', $href));
-		$arguments['href'] = format_ascii($href);
-	} elseif (format_text_starts('javascript:', $href)) {
-		//fix link for javascript
-		$arguments['href'] = '#';
-		$arguments['onclick'] = $href;
-	} elseif ($href) {
-		$arguments['href'] = htmlentities($href);
-	} else {
-		array_argument($arguments, 'empty');
-	}
-	if ($new_window) $arguments['target'] = '_blank';
-	return draw_tag('a', $arguments, $link);
-}
-
-function html_article() {
+class HTML {
+	private static $instance;
 	
+	private function __construct() {
+
+	}
+	
+	public static function getInstance() {
+		if (is_null(self::$instance)) self::$instance = new self();
+		return self::$instance;
+	}
+	
+	public function div($content=false, $class_args=false) {
+		return tag('div', $content, $class_args);
+	}
+	
+	private function tag($tag, $content=false, $class=false) {
+	
+	}
 }
+
+/*
+$html->p('title',
+	'Some text' . $html->strong(false, 'Michael')
+);
+*/
